6280b47122f483b1741dfc86195e2ec0936f6785
unknown <00087717@uca.edu.sv>
2018-09-28 23:44:20 -0600

    Final

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,5 +1,5 @@
 
-    prompt("Metros <=> Centímetros |  Kilómetros <=> Metros | Pies <=> Metros |   Celsius <=> Fahrenheit | Kelvin <=> Fahrenheit |  Kelvin <=> Celsius.")
+prompt("---- CONVERTIDOR ----  Metros <=> Centímetros |  Kilómetros <=> Metros | Pies <=> Metros |   Celsius <=> Fahrenheit | Kelvin <=> Fahrenheit |  Kelvin <=> Celsius.  --> Ingrese cualquier tecla" )
 
 function MenuConver(num, uni_tengo, unidad_quiero, tipo) {
 
@@ -37,10 +37,10 @@ function MenuConver(num, uni_tengo, unidad_quiero, tipo) {
 
                     default:
 
-                    console.log("cm = centimentros | km = kilometros | pie = pies")
+                        console.log(" Si quiere de m a ----> cm = centimentros | km = kilometros | pie = pies")
                         break;
                 }
-            break;
+                break;
             }
 
             if (unidad_quiero == "m") {
@@ -67,24 +67,84 @@ function MenuConver(num, uni_tengo, unidad_quiero, tipo) {
 
                     default:
 
-                    console.log("cm = centimentros | km = kilometros | pie = pies")
+                        console.log(" Si quiere convertir a m ----> cm = centimentros | km = kilometros | pie = pies")
                         break;
                 }
             }
 
             else {
-                console.log("POR FAVOR ESCRIBIR LAS MEDIDAS CORRESPONDIENTES")
+                console.log(" m = metros | cm = centímetros | km = kilometros | pie = pies")
             }
 
         case "t":
+            if (uni_tengo == "k") {
+                switch (unidad_quiero) {
+                    case "c":
+                        unidad = num-273.15;
+                        console.log(unidad + unidad_quiero)
+                        break;
+
+                    case "f":
+                        unidad =  ((9/5)*(num-273.15))+32;
+                        console.log(unidad + unidad_quiero)
+
+                        break;
+
+
+                        default:
+                        console.log(" Si quiere de Kelvin a --> c = celsius | f = fahrenheit")
+                        break;
+                }
+            }
+
+            if (uni_tengo == "f") {
+                switch (unidad_quiero) {
+                    case "c":
+                        unidad = (5/9)*(num-32);
+                        console.log(unidad + unidad_quiero)
+                        break;
+
+                    case "k":
+                        unidad =  ((5/9)*(num-32))+273.15;
+                        console.log(unidad + unidad_quiero)
+
+                        break;
+
+
+                        default:
+                        console.log("Si quiere de fahrenheit a -->  c = celsius | k = kelvin")
+                        break;
+                }
+            }
 
 
 
-        
+            if (uni_tengo == "c") {
+                switch (unidad_quiero) {
+                    case "k":
+                        unidad = num + 273.15;
+                        console.log(unidad + unidad_quiero)
+                        break;
+
+                    case "f":
+                        unidad =  ( (9/5)*num)+32;
+                        console.log(unidad + unidad_quiero)
+
+                        break;
+
+
+                        default:
+                        console.log(" Si quiere de Celsius a --> k = kelvin | f = fahrenheit")
+                        break;
+                }
+            }
             break;
 
 
+
         default:
+
+        console.log( " Escriba  l = longitud | t = temperatura")
             break;
     }
 }
